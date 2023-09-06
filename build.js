a1d39bd17df66dc6559321fb6006cea64accbb73
Morgan Engel <morgan.engel@gusto.com>
2023-09-06 14:35:53 -0700

    update to fix template and move output into a confirm for easy copying / pasting

--- build.js
+++ build.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 
 exec('npx uglifyjs bookmarklet.js', function(e, code){
-	var template = fs.readFileSync('selector.template', 'utf8');
+	var template = fs.readFileSync('template.html', 'utf8');
 	code = code.replace(/'/g, '\\\'');
 	code = code.replace(/\s*$/g, '');
 	code = code.replace('my secret password', "'+target.value+'");
-	code = code.replace('javascript:', 'javascript:/*ALGORITHMIC PASSWORD GENERATOR*/');
+	code = `javascript:/*ALGORITHMIC PASSWORD GENERATOR*/${code}`;
 	code = template.replace('template', code);
 	fs.writeFileSync('index.html', code);
 	console.log("index.html has been updated");
